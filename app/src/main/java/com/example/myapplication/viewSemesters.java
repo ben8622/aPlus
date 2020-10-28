dc58b6cf8edfc7248c76e718ec9f360355c41793
Ben <benjaminjknight0987@gmail.com>
2020-10-28 18:56:17 -0500

    added blank expandableListView on viewSemesters activity

--- app/src/main/java/com/example/myapplication/viewSemesters.java
+++ app/src/main/java/com/example/myapplication/viewSemesters.java
@@ -2,13 +2,73 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.widget.ExpandableListView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class viewSemesters extends AppCompatActivity {
 
+    ExpandableListView expandableListView;
+    List<String> listGroup;
+    HashMap<String, List<String>> listItem;
+    MainAdapter adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_semesters);
+
+
+        expandableListView = findViewById(R.id.expandable_listview);
+        listGroup = new ArrayList<>();
+        listItem = new HashMap<>();
+        adapter = new MainAdapter( this, listGroup, listItem);
+        expandableListView.setAdapter((adapter));
+
+        initListData();
+    }
+
+    public void initListData(){
+        listGroup.add(getString(R.string.semester1));
+        listGroup.add(getString(R.string.semester2));
+        listGroup.add(getString(R.string.semester3));
+        listGroup.add(getString(R.string.semester4));
+
+        String[] array;
+
+        List<String> list1 = new ArrayList<>();
+        array = getResources().getStringArray(R.array.semester1);
+        for (String item : array) {
+            list1.add(item);
+        }
+
+        List<String> list2 = new ArrayList<>();
+        array = getResources().getStringArray(R.array.semester2);
+        for (String item : array) {
+            list2.add(item);
+        }
+
+        List<String> list3 = new ArrayList<>();
+        array = getResources().getStringArray(R.array.semester3);
+        for (String item : array) {
+            list3.add(item);
+        }
+
+        List<String> list4 = new ArrayList<>();
+        array = getResources().getStringArray(R.array.semester4);
+        for (String item : array) {
+            list4.add(item);
+        }
+
+        listItem.put(listGroup.get(0), list1);
+        listItem.put(listGroup.get(1), list2);
+        listItem.put(listGroup.get(2), list3);
+        listItem.put(listGroup.get(3), list4);
+        adapter.notifyDataSetChanged();
     }
 }
